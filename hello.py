22de92c377e5e217e493fe4365cc06bb307deaeb
jatinsingh786 <48169218+jatinsingh786@users.noreply.github.com>
2019-03-17 05:48:15 -0400

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,25 @@
+print("Hello World")
+
+# Addition
+a = 4
+b = 5
+c = a + b
+print(c)
+
+# Substraction
+a = 5
+b = 3
+c = a - b
+print(c)
+
+# Multiplication
+a = 5
+b = 5
+c = a * b
+print(c)
+
+# Division
+a = 6
+b = 2
+c = a / b
+print(c)
